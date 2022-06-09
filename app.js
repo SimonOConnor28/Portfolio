61e3ae066ba2db0d2edb2251556e1ce5b0b6d091
SimonOConnor28 <simonoconnor28@gmail.com>
2022-06-08 21:25:03 -0400

    add projects section and found some javascript for nav bar to change background when scrolled

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('hello');
+
+    const x_mainNav=document.getElementById("navBarId");
+    const x_navItems=document.querySelectorAll("#navBarId nav a");
+    const x_headerHeight = 100;
+
+    window.addEventListener("scroll",(x)=>{
+        if(window.pageYOffset >= window.innerHeight - x_headerHeight)
+            x_mainNav.style.background="black";
+        else
+            x_mainNav.style.background="transparent";
+    });
+
+
